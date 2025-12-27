03735638fc117a065647c4c2cc1475ae15e4961c
Valvemapfile <123307806+Valvemapfile@users.noreply.github.com>
2025-12-27 17:05:21 -0600

    Add files via upload

--- noptoepub-SOURCE.py
+++ noptoepub-SOURCE.py
@@ -3,11 +3,10 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import turtle
 from ebooklib import epub
+from datetime import datetime, timezone
 import time
-import sys
 
 f = open("log.txt", "w")
-
 
 screen = turtle.Screen()
 screen.bgpic("bg.png")
@@ -15,77 +14,82 @@ screen.setup(width=800, height=600)
 screen.title("Window will close once program is finished")
 
 def find_next_link(response):
-
     soup = BeautifulSoup(response.text, "html.parser")
-
     for a in soup.find_all("a"):
         if a.get_text(strip=True).lower() == "next":
             href = a.get("href")
             if href:
                 return urljoin(response.url, href)
-            else:
-                return None
-
     return None
 
 def extract_article_body(response):
     soup = BeautifulSoup(response.text, "html.parser")
 
+    name = soup.find("a", class_="subreddit-name", href=lambda h: h and h.startswith("/r/"))
+    subreddit = name.get_text(strip=True) if name else "Unknown Subreddit"
+
+    author_tag = soup.find("a", class_="author-name", href=lambda h: h and h.startswith("/user/"))
+    author = author_tag.get_text(strip=True) if author_tag else "Unknown Author"
+
+
+    h1 = soup.find("h1")
+    title = h1.get_text(strip=True) if h1 else "Untitled"
+
     div = soup.find("div", attrs={"property": "schema:articleBody"})
-
     if not div:
-        f.write("Target div not found\n")
-        return "", ""
+        return "", subreddit, author, title
 
-    html_lines = []
-    text_lines = []
+    html_lines = [f"<h1>{title}</h1>"]
 
     for child in div.children:
-        # Skip whitespace
-        if getattr(child, "name", None) is None:
-            continue
+        if getattr(child, "name", None):
+            html_lines.append(str(child))
+        elif isinstance(child, str) and child.strip():
+            html_lines.append(f"<p>{child.strip()}</p>")
 
-        # If it's already a paragraph or block, keep it
-        if child.name in ("p", "blockquote", "ul", "ol"):
-            html = str(child)
-            text = child.get_text(strip=True)
+    html = "\n".join(html_lines)
+    return html, subreddit, author, title
 
-        # Otherwise wrap it
-        else:
-            html = f"<p>{child.decode_contents()}</p>"
-            text = child.get_text(strip=True)
-
-        html_lines.append(html)
-        text_lines.append(text)
-
-    return "\n".join(text_lines), "\n".join(html_lines)
-
-start_url = turtle.textinput("Enter Url", "Enter FULL reddit url of first chapter (eg https://www.reddit.com/r/HFY/comments/u19xpa/the_nature_of_predators/)")
+start_url = turtle.textinput(
+    "Enter Url",
+    "Enter FULL reddit url of first chapter (eg https://www.reddit.com/r/HFY/comments/u19xpa/the_nature_of_predators/)"
+)
 if start_url == "":
     f.write("No URL entered\n")
-book = epub.EpubBook()
-book_title = turtle.textinput("Enter Title", "Enter title of the EPUB (If left blank the epub will be titled untitled.epub)")
+    raise SystemExit
+
+book_title = turtle.textinput(
+    "Enter Title",
+    "Enter title of the EPUB (If left blank the epub will be titled untitled.epub)"
+)
 if book_title == "":
     book_title = "untitled"
     f.write("Book title not entered, set to untitled\n")
-book.set_title(book_title)
-book.set_language("en")
-book_cover = turtle.textinput("Enter Cover Url", "Enter url of cover image (right click image -> copy image link) (If left blank cover will be blank)")
+
+book_cover = turtle.textinput(
+    "Enter Cover Url",
+    "Enter url of cover image (right click image -> copy image link) (If left blank cover will be blank)"
+)
 if book_cover == "":
     book_cover = "https://i.imgur.com/JegySXD.png"
     f.write("Book cover not entered, set to blank\n")
+
+book = epub.EpubBook()
+book.set_title(book_title)
+book.set_language("en")
+book.set_identifier(start_url)
 
 data = requests.get(url=book_cover).content
 g = open('cover.jpg','wb')
 g.write(data)
 g.close()
 
-
 img = epub.EpubImage()
 img.file_name = "images/pic.jpg"
 img.media_type = "image/jpeg"
 img.content = data
-
+book.add_item(img)
+book.set_cover("img.jpg", data)
 
 nav_css = epub.EpubItem(
     uid="style_nav",
@@ -93,12 +97,13 @@ nav_css = epub.EpubItem(
     media_type="text/css",
     content="body { font-family: serif; margin: 5%; line-height: 1.6; }",
 )
+book.add_item(nav_css)
 
 chapter_list = []
 start_time = time.time()
 iteration_count = 0
 
-for i in range(1, 99999):
+while start_url:
     iteration_count += 1
     response = requests.get(start_url, timeout=10)
     if response.status_code == 429:
@@ -107,36 +112,42 @@ for i in range(1, 99999):
     elif response.status_code != 200:
         f.write("Failed to retrieve page, Reddit is probably down right now, try again later :3\n")
         break
-    text, html = extract_article_body(response)
-    i = epub.EpubHtml(title="Chapter " + str(i), file_name=str(i) + ".xhtml", lang="en")
-    i.content = html
-    chapter_list.append(i)
+
+    html, subreddit, author, title = extract_article_body(response)
+
+    ch = epub.EpubHtml(
+        title=title,
+        file_name=str(len(chapter_list)+1) + ".xhtml",
+        lang="en"
+    )
+    ch.content = html or "<p>Empty chapter</p>"
+    chapter_list.append(ch)
+
     start_url = find_next_link(response)
-    if time.time() - start_time > 60 == 0:
-        iteration_count = 0
-        start_time = time.time()
+
     if iteration_count == 99:
         time.sleep(60 - (time.time() - start_time))
         iteration_count = 0
         start_time = time.time()
+
     if start_url:
         f.write("Next link found\n")
     else:
         f.write("Next link not found\n")
         break
+
     screen.update()
 
-for i in range(len(chapter_list)):
-    book.add_item(chapter_list[i])
-book.add_item(img)
-book.add_item(nav_css)
-book.set_cover("img.jpg", open('cover.jpg', 'rb').read())
+for ch in chapter_list:
+    book.add_item(ch)
 
-book.toc = tuple(epub.Link(chapter_list[i].file_name, chapter_list[i].title, chapter_list[i].file_name) for i in range(len(chapter_list)))
+book.add_item(epub.EpubNav())
+book.add_item(epub.EpubNcx())
+book.toc = tuple(epub.Link(ch.file_name, ch.title, ch.file_name) for ch in chapter_list)
+book.spine = ["nav"] + chapter_list
 
-for i in range(len(chapter_list)):
-    book.spine.append(chapter_list[i])
-
+book.add_metadata("DC", "creator", author)
+book.add_metadata("DC", "publisher", subreddit)
 
 epub.write_epub(str(book_title) + ".epub", book)
 f.write("EPUB created successfully\n")
